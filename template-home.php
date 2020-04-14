7f13093bb2c3635042ab3adc5c929a5e35cb357f
Jordi M <jordi.marquez@eram.cat>
2020-04-14 17:50:33 +0200

    Create template-home.php

---
+++ template-home.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ Template Name: HomePage
+ */
+
+get_header();
+?>
+
+ <div class="container">
+    <div class="row home align-items-center">
+        <div class="col-12 text-center">
+
+		<?php
+            //comença el loop
+		while ( have_posts() ) :
+			the_post();
+            //the_content mostra el contingut de la pàgina (dins del backend)
+			the_content();
+
+		endwhile; // End of the loop.
+		?>
+
+        </div>
+    </div>
+</div>
+
+<?php
+get_footer();
